1e9edce8088c5763881ea78fc3000d3c83d89550
joseph <j.hernandez.92189@gmail.com>
2022-10-10 13:37:37 -0700

    added base get / post routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,39 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const notes = require('./db/db.json');
+
+
 //body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+// app.post('/api/notes', (req, res) => {
+//     const note = body;
+
+//     fs.writeFileSync(
+//         path.join(__dirname, './db/db.json'),
+//     )
+// });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+console.log(notes);
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
